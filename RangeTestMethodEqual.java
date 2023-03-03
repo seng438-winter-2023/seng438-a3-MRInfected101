f5f8afcca207ceac54a9d2af5eb36437dc9c5d82
NicoleZac <80605815+NicoleZac@users.noreply.github.com>
2023-03-02 18:10:03 -0700

    Create RangeTestMethodEqual.java
    Added 1 new test

---
+++ RangeTestMethodEqual.java
@@ -0,0 +1,45 @@
+package org.jfree.data.test;
+import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.*;
+public class RangeTestMethodEqual {
+private Range exampleRange1, exampleRange2, exampleRange3, exampleRange4, exampleRange5;
+@BeforeClass public static void setUpBeforeClass() throws Exception {
+}
+@Before
+public void setUp() throws Exception {
+	exampleRange1 = new Range(5, 10);
+	exampleRange2 = new Range(-2, 3);
+	exampleRange3 = null;
+	exampleRange4 = new Range(5,10);
+	exampleRange5 = new Range(5,7);
+
+}
+
+/*
+* Tests for method Equals
+* 3 tests
+*/
+	@Test
+	public void testEqualsWithEquivalentObject() {
+		assertTrue("Expected ranges to be equal", exampleRange1.equals(exampleRange4));
+	}
+	@Test
+	public void testEqualsWithNonEquivalentObject() {
+		assertFalse("Expected ranges to not be equal", exampleRange2.equals(exampleRange1));
+	}
+	@Test
+	public void testEqualsWithNullObject() {
+		assertFalse("Expected ranges to not be equal because of a null range",exampleRange1.equals(exampleRange3));	
+	}
+	@Test
+	public void testEqualsWithPartialMatch() {
+		assertFalse("Expected ranges to not be equal because of a null range",exampleRange1.equals(exampleRange5));	
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+}
